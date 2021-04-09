a015984fbaf7446ab78b135a1c9f930e1b2bc1b0
Beat Schärz <beatschaerz@hotmail.com>
2021-04-09 10:49:48 +0200

    added tests

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+def randy(fr, to):
+    import random
+    return random.randint(fr, to)
+
+
 @app.route('/')
 def hello():
     return 'Hello World!'
+
 
 @app.route('/cc/<text>')
 def cc(text):
     return str(len(text)) + " characters!"
 
+
 @app.route('/random/<fr>/<to>')
 def random(fr, to):
-    import random
     try:
-        return str(random.randint(int(fr), int(to)))
-    except:
+        return str(randy(int(fr), int(to)))
+    except ValueError:
         return "ERROR"
